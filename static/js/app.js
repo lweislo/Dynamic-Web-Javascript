a69e1e5471a175ebe09455159ab197541d83b22c
Laura Weislo <laura@weislo.com>
2019-03-05 17:31:53 -0500

    added partial text and shape

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,8 @@ var data = data;
 var uniqueDate = []
 var uniqueCity = []
 var uniqueState = []
+var uniqueCountry = []
+var uniqueShape = []
 //====================
 // To convert the table data to proper case
 String.prototype.toProperCase = function () {
@@ -25,6 +27,12 @@ if (key === "datetime") {
   else if (key === "state") {
     if (!uniqueState.includes(value)) {
     uniqueState.push(value);}}
+  else if (key === "country") {
+    if (!uniqueCountry.includes(value)) {
+    uniqueCountry.push(value);}}
+  else if (key === "shape") {
+    if (!uniqueShape.includes(value)) {
+    uniqueShape.push(value);}}
   });
 });
 // Populate the dropdown list itself
@@ -48,53 +56,126 @@ uniqueState.forEach((state) => {
   .text(state)
   .on("click", filterChange);
 });
+//=============COUNTRY=======================
+uniqueCountry.sort()
+var countryDdl = d3.select("#country-ddl")
+uniqueCountry.forEach((country) => {
+  country = country.toUpperCase();
+  var listitem = countryDdl.append("a")
+  .attr("class","dropdown-item")
+  .attr("value", "country")
+  .text(country)
+  .on("click", filterChange);
+});
+//=============SHAPE========================
+uniqueShape.sort()
+var shapeDdl = d3.select("#shape-ddl")
+uniqueShape.forEach((shape) => {
+  shape = shape.toProperCase();
+  var listitem = shapeDdl.append("a")
+  .attr("class","dropdown-item")
+  .attr("value", "shape")
+  .text(shape)
+  .on("click", filterChange);
+});
 //============CITY=======================
 var cityBtn = d3.select("#city-btn")
 cityBtn.on("click", filterChange)
 
 //=========================================
-// Showing all data from bottom link
+// Show all data from bottom link
 var moreData = d3.select("#table-end")
 moreData.on("click", allData);
-//====================
+//=========================================
+//Check for partial text searching
+var partialTextBox = false;
+var partText = d3.select("#text-check");
+partText.on("change", partialText);
+//=========================================
+function partialText() {
+  if (partialTextBox) {
+    partialTextBox = false;
+  }
+  else {
+    partialTextBox = true
+  }
+  console.log("Is checked", partialTextBox)
+}
+//=======================================
+//        FILTER SECTION
+//=======================================
 function filterChange(event) {
   d3.event.preventDefault();
-  var inputText = d3.select(this).property("text");
-  var inputOrigin = d3.select(this).attr("value");
-  var inputId = d3.select(this).attr("id");
-  console.log(inputText, inputOrigin, inputId)
-  if (inputId !== null) {
-    inputText = d3.select("#city-text").property("value");
-    inputText = inputText.toLowerCase();
-  var filteredData = data.filter(item => item.city === inputText);}
-  else if (inputOrigin === "datetime") {
-  var filteredData = data.filter(item => item.datetime === inputText);}
+  // Check the text, value and id of the triggering element
+  // Incoming is either from dropdown (select) or open text (two)
+  let inputSelect = d3.select(this).property("text");
+  let cityText = d3.select("#city-text").property("value");
+  let comText = d3.select("#comment-text").property("value");
+  //Did the event come from a dropdown?
+  //If so make the variable all lowercase
+  if (inputSelect){
+    inputSelect = inputSelect.toLowerCase();
+  }
+  // Get the name of the element that raised the event
+  inputOrigin = d3.select(this).attr("value");
+  inputId = d3.select(this).attr("id");
+
+  //If it's open text from City also make lowercase
+  if (cityText) {
+    cityText = cityText.toLowerCase();
+    inputOrigin = d3.select(this).attr("id")}
+
+// Filter statements here
+  if (inputOrigin === "datetime") {
+  filteredData = data.filter(item => item.datetime === inputSelect);
+  }
+  //State dropdown filter
   else if (inputOrigin === "state") {
-  var filteredData = data.filter(item => item.state === inputText);}
-//Check to see if there are any results!
-  resultsLen = filteredData.length;
-  if (resultsLen === 0 && inputId === "city-btn"){
-    var tmsg = d3.select("#result-msg");
-    tmsg.append("p").text("There were no results for that exact selection, but here are partial matches.");
-    inputText = inputText.toLowerCase();
-    var filteredData = data.filter(item => item.city.includes(inputText));
-}
+  filteredData = data.filter(item => item.state === inputSelect);
+  }
+    //Country dropdown filter
+  else if (inputOrigin === "country") {
+    var filteredData = data.filter(item => item.country === inputSelect);
+  }
+    //Shape dropdown filter
+  else if (inputOrigin === "shape") {
+    var filteredData = data.filter(item => item["shape"] === inputSelect);
+  }
+  else if (inputId === "city-btn") {
+  // City text field
+    if (partialTextBox) {
+      filteredData = data.filter(item => item.city.includes(cityText));
+    }
+    else {
+      var filteredData = data.filter(item => item.city === cityText);
+    }
+  }
   //write partial search here
-  drawTable(filteredData);
+  drawTable(filteredData, inputId);
   //Then go on and insert rows/columns into table using D3
 }
 //===================
+// DRAW TABLE
+//===================
 // Get a reference to the table body
-function drawTable(filteredData) {
-  resultsLen = filteredData.length;
-  var tmsg = d3.select("#result-msg");
+function drawTable(filteredData, inputId) {
   var tbody = d3.select("tbody");
-  var tdiv = d3.select("#table-area");
+  var tmsg = d3.select("#result-msg");
+  resultsLen = filteredData.length;
   tbody.html("");
+  tmsg.html("")
     if (resultsLen === 0) {
       tmsg.append("p").text("There were no results for that selection.");
       defaultTable();
     }
+    //Check to see if there are any results!
+
+    //   console.log(resultsLen);
+    // else if (resultsLen === 0 && inputId === "city-btn") {
+    //     var tmsg = d3.select("#result-msg");
+    //     tmsg.append("p").text("There were no results for that exact selection, but here are partial matches.");
+    //     filteredData = data.filter(item => item.city.includes(cityText));
+    // }
 // Add the rows and cells of data from the data.js source file.
   filteredData.forEach((ufoSighting) => {
   var row = tbody.append("tr");
@@ -106,9 +187,8 @@ function drawTable(filteredData) {
       value = value.toUpperCase();}
     else if (key === "comments") {
       value = stripHtml(value); }
-    // else if (key === "durationMinutes") {
-    //   durationParse(value)
-    // }
+    else if (key === "shape") {
+      value = value.toProperCase();}
     var cell = tbody.append("td");
     cell.text(value);
   }); //end of forEach 2
